b483290512b494ebedd0e6d077e3a67aee335190
Logosph <zaklyakov2004@ya.ru>
2024-02-03 22:39:12 +0300

    Editing usecase

--- python/domain/ApplicationRunUseCase.py
+++ python/domain/ApplicationRunUseCase.py
@@ -7,3 +7,13 @@
 
 def execute():
     pass
+
+# Блоки -> код на Python -> Запускается этот код
+
+# set_digital = 0.8
+# beginfor from 10 to 20 -> for i in range(10, 20):
+# <code>
+# endfor
+# beginif <cond> -> if <cond>:
+# <code>
+# endif
